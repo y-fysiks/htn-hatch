a86a5663039c748aafa7cf0730a81a2565ddb43a
Yubo Wang <yubo0809@outlook.com>
2022-09-17 19:32:31 -0400

    moved stuff to the right spots

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
-const { Client } = import("pg");
-
 import React from 'react';
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from './Home/Home';
 import Matches from './Matches/Matches';
 import Profile from './Profile/Profile';
@@ -12,12 +8,15 @@ import Team from './Team/Team';
 import Explore from './Explore/Explore';
 import  { Navbar } from './Navbar/Navbar';
 
+const { Client } = import("pg");
+
 const client = new Client({
     connectionString: "postgresql://carl:8lxP-wTqhsa-d6J4YVxUSA@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dhtn-hatch-5024",
     application_name: "$ docs_quickstart_node"
 });
 
 function App() {
+  
   return (
     <BrowserRouter>
       <Navbar/>
