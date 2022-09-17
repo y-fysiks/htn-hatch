773fb08b19b99c0f3244a849c7b0f47fda28a25e
Carl Zhang <zhang.carl.x@gmail.com>
2022-09-17 18:57:03 -0400

    Revert "database link"
    This reverts commit bf9965ed8def3b8eab04f77db51ba0e5bbf32d86.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
-const { Client } = import("pg");
 
 import React from 'react';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 import Home from './Home/Home';
 import Matches from './Matches/Matches';
 import Profile from './Profile/Profile';
 import Team from './Team/Team';
 import Explore from './Explore/Explore';
-import  { Navbar } from './Navbar/Navbar';
+import  { Navbar } from './Navbar/Navbar'
 
-const client = new Client({
-    connectionString: "postgresql://carl:8lxP-wTqhsa-d6J4YVxUSA@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dhtn-hatch-5024",
-    application_name: "$ docs_quickstart_node"
-});
 
 function App() {
   return (
