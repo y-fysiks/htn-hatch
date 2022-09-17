75207845fa013a2c448b131eaa9ad93ff6a194a5
Yubo Wang <yubo0809@outlook.com>
2022-09-17 15:20:04 -0400

    created matches page

---
+++ client/src/Matches/Matches.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//Styled Components
+import {
+  SampleDiv
+} from './Matches.elements'
+
+const Home = () => {
+  return (
+    <>
+      <SampleDiv>This is Matches</SampleDiv>
+    </>
+  )
+}
+
+export default Home
