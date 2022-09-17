d2ac74e26d023de6c58deec4eb36a56ddb3d9f4f
Carl Zhang <zhang.carl.x@gmail.com>
2022-09-17 19:42:34 -0400

    backend?

--- server/index.js
+++ server/index.js
@@ -3,3 +3,15 @@ const express = require("express");
 const { Client } = require("pg");
 const cors = require("cors");
 require("dotenv").config();
+
+// initialize express
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(express.json());
+app.use(cors());
+
+// access to db
+const db = new Client({
+    connectionString: "postgresql://carl:WBFMMmhrJWAJXjAV56zA3w@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dhtn-hatch-5024",
+    application_name: "$ docs_quickstart_node"
+});
