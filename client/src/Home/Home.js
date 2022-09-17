be50df57bb3d8aea5b85578cf5637a59e643e38c
Ri-Hong <riri.hong@gmail.com>
2022-09-17 15:05:09 -0400

    1/5th done navbar

---
+++ client/src/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//Styled Components
+import {
+  SampleDiv
+} from './Home.elements'
+
+const Home = () => {
+  return (
+    <>
+      <SampleDiv>This is Home</SampleDiv>
+      
+    </>
+  )
+}
+
+export default Home
