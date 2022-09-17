93955d9b7c34d44597d181e9c94e856e1aecbd5f
Carl Zhang <zhang.carl.x@gmail.com>
2022-09-17 19:51:37 -0400

    routing

--- server/index.js
+++ server/index.js
@@ -15,3 +15,12 @@ const db = new Client({
     connectionString: "postgresql://carl:WBFMMmhrJWAJXjAV56zA3w@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dhtn-hatch-5024",
     application_name: "$ docs_quickstart_node"
 });
+
+// fetch user
+// fetch team
+// merge team a/team b
+// update team
+// delete team
+// post user?
+// post team?
+// update user?
