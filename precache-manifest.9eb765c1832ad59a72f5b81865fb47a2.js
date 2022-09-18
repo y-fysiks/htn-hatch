2e96ce52ee9355147fdedaea7befc56373a406f5
Ri-Hong <riri.hong@gmail.com>
2022-09-18 02:59:04 -0400

    Updates

---
+++ precache-manifest.9eb765c1832ad59a72f5b81865fb47a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fa9e63048f4370bceea",
+    "url": "/htn-hatch/static/css/main.42a8cc13.chunk.css"
+  },
+  {
+    "revision": "5fa9e63048f4370bceea",
+    "url": "/htn-hatch/static/js/main.5fa9e630.chunk.js"
+  },
+  {
+    "revision": "2d281c672a566deb1a05",
+    "url": "/htn-hatch/static/js/1.2d281c67.chunk.js"
+  },
+  {
+    "revision": "e59ac1e97eadb495a98d",
+    "url": "/htn-hatch/static/js/2.e59ac1e9.chunk.js"
+  },
+  {
+    "revision": "2d80c353697a66d5f603",
+    "url": "/htn-hatch/static/js/runtime~main.2d80c353.js"
+  },
+  {
+    "revision": "90bc5ea6e173fa240fbe8eacb9097973",
+    "url": "/htn-hatch/static/media/sampleLogo.90bc5ea6.png"
+  },
+  {
+    "revision": "6b887f01bd300060ca6615a939ac816b",
+    "url": "/htn-hatch/index.html"
+  }
+];
